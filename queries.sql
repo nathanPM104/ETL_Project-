4cf3956445dbfd64a1ba9a18fd2d071a1acef550
Maria Eliza Medeiros Barros <eliza.medeiros@gmail.com>
2021-06-12 14:33:28 -0700

    team colaboration

--- queries.sql
+++ queries.sql
@@ -1,6 +1,12 @@
 -- Drop table if exists
+<<<<<<< HEAD
+--DROP TABLE coffee_df_2016;
+--DROP TABLE HappinessAlcohol_df;
+--DROP TABLE suicide_df_2016;
+=======
 DROP TABLE coffee_df_2016;
 DROP TABLE HappinessAlcohol_df;
+>>>>>>> 5ea999050208ca721ad8f3551df35101070e801a
 
 -- Create new table
 CREATE TABLE coffee_df_2016 (
@@ -31,7 +37,18 @@ CREATE TABLE suicide_df_2016(
 	Country VARCHAR PRIMARY KEY,
 	SuicideCount INT
 );
+<<<<<<< HEAD
+
+-- View table columns and datatypes
+SELECT * FROM suicide_df_2016;
+=======
+>>>>>>> 5ea999050208ca721ad8f3551df35101070e801a
 
 -- View table columns and datatypes
 SELECT * FROM suicide_df_2016;
 
+SELECT suicide_df_2016.country, suicide_df_2016.suicidecount, HappinessAlcohol_df.happinessscore, coffee_df_2016.coffee_consumption  FROM suicide_df_2016
+LEFT JOIN HappinessAlcohol_df
+ON suicide_df_2016.country = HappinessAlcohol_df.country
+LEFT JOIN coffee_df_2016
+ON HappinessAlcohol_df.country = coffee_df_2016.country;
