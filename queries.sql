5ea999050208ca721ad8f3551df35101070e801a
nathanPM104 <79662158+nathanPM104@users.noreply.github.com>
2021-06-12 13:01:48 -0700

    suicide tables

--- queries.sql
+++ queries.sql
@@ -2,17 +2,14 @@
 DROP TABLE coffee_df_2016;
 DROP TABLE HappinessAlcohol_df;
 
-
 -- Create new table
 CREATE TABLE coffee_df_2016 (
 	Country VARCHAR PRIMARY KEY,
 	Coffee_Consumption INT
 );
 
-
 -- View table columns and datatypes
 SELECT * FROM coffee_df_2016;
-
 
 -- Create new table
 CREATE TABLE HappinessAlcohol_df(
@@ -25,8 +22,16 @@ CREATE TABLE HappinessAlcohol_df(
 	Wine_PerCapita INT 
 );
 
+
 -- View table columns and datatypes
 SELECT * FROM HappinessAlcohol_df;
 
+-- Create new table
+CREATE TABLE suicide_df_2016(
+	Country VARCHAR PRIMARY KEY,
+	SuicideCount INT
+);
 
+-- View table columns and datatypes
+SELECT * FROM suicide_df_2016;
 
